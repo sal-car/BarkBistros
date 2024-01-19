c920143514dbde1539fa505ae3fae9a5d121b84b
sal-car <sally.carlund@gmail.com>
2024-01-19 14:12:02 +0100

    style: basic searchForm & toggleSwitch with MaterialUI

--- app/imports/ui/components/SearchForm.tsx
+++ app/imports/ui/components/SearchForm.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { TextField, Switch, FormControlLabel } from '@mui/material';
 
 export const SearchForm = () => (
-  <div>
-    <h1>Searchform</h1>
-  </div>
+  <form className="flex flex-col gap-5 items-center">
+    <div className="w-[80vw]">
+      <TextField
+        label="Name or keyword"
+        type="text"
+        variant="standard"
+        color="primary"
+        fullWidth
+      />
+    </div>
+    <FormControlLabel control={<Switch name="open-now" />} label="Open now" />{' '}
+  </form>
 );
