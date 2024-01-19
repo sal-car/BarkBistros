78bc11de60ea4bac6fd9d3bc0c8ff46fb1093bc6
sal-car <sally.carlund@gmail.com>
2024-01-19 14:52:19 +0100

    style: added dark mode theme provider

--- app/imports/ui/App.tsx
+++ app/imports/ui/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import { ViewRestaurants } from './containers/ViewRestaurants';
+import { CssBaseline } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#FAFAFB',
+    },
+  },
+});
 
 export const App = () => (
-  <div className="App ">
-    <ViewRestaurants />
-  </div>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <div className="App  ">
+      <ViewRestaurants />
+    </div>
+  </ThemeProvider>
 );
