fe4a27f2b6d3d765c967f1de0c7a49219dd3bc5c
sal-car <sally.carlund@gmail.com>
2024-01-20 13:22:06 +0100

    style: add header

---
+++ app/imports/ui/containers/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => (
+  <div className="bg-aubergine mb-8 p-5 flex-col flex  items-center">
+    <h1 className="  text-4xl  font-logo mb-1 sm:text-5xl">BarkBistros🐕</h1>
+    <h3 className=" text-xs sm:text-sm font-light">
+      Restaurants for Foodies and Furry Friends
+    </h3>
+  </div>
+);
