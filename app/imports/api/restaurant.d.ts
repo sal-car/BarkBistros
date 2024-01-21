146159bf21160e07a13fb7d401dec7f36860ba2f
sal-car <sally.carlund@gmail.com>
2024-01-21 17:52:51 +0100

    chore: type declaration for Restaurant

--- app/imports/api/restaurant.d.ts
+++ app/imports/api/restaurant.d.ts
@@ -1,1 +1,21 @@
-// Declare interface Restaurant
+declare global {
+  interface Restaurant {
+    name: string;
+    address: string;
+    tags: string[];
+    creation_date: Date;
+    opening_hours: OpeningHours;
+  }
+}
+
+interface OpeningHours {
+  monday: string;
+  tuesday: string;
+  wednesday: string;
+  thursday: string;
+  friday: string;
+  saturday: string;
+  sunday: string;
+}
+
+export {};
