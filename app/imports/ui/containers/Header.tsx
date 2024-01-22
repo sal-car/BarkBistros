bbcdc074638bbdbd7e17b28bce163093f7dcf809
sal-car <sally.carlund@gmail.com>
2024-01-22 12:11:39 +0100

    refactor: material ui syntax instead of HTML

--- app/imports/ui/containers/Header.tsx
+++ app/imports/ui/containers/Header.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Box } from '@mui/material';
 
 export const Header = () => (
-  <div className="bg-aubergine mb-8 p-5 flex-col flex  items-center">
+  <Box className="bg-aubergine mb-8 p-5 flex-col flex  items-center">
     <h1 className="  text-4xl  font-logo mb-1 sm:text-5xl">BarkBistros🐕</h1>
     <h3 className=" text-xs sm:text-sm font-light">
       Restaurants for Foodies and Furry Friends
     </h3>
-  </div>
+  </Box>
 );
