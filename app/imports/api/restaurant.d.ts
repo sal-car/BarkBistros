00bf949969e6f42812373798bf429afff013db34
sal-car <sally.carlund@gmail.com>
2024-01-22 12:10:54 +0100

    fix: index signature for OpeningHours

--- app/imports/api/restaurant.d.ts
+++ app/imports/api/restaurant.d.ts
@@ -6,16 +6,17 @@ declare global {
     creation_date: Date;
     opening_hours: OpeningHours;
   }
-}
 
-interface OpeningHours {
-  monday: string;
-  tuesday: string;
-  wednesday: string;
-  thursday: string;
-  friday: string;
-  saturday: string;
-  sunday: string;
+  interface OpeningHours {
+    monday: string;
+    tuesday: string;
+    wednesday: string;
+    thursday: string;
+    friday: string;
+    saturday: string;
+    sunday: string;
+    [key: string]: string;
+  }
 }
 
 export {};
