0442a275e10fca1c510294ebeb43f1178f513bb8
sal-car <sally.carlund@gmail.com>
2024-01-24 22:32:09 +0100

    fix: run publications regardeless of fixture status

--- app/server/main.ts
+++ app/server/main.ts
@@ -2,17 +2,18 @@ import { Meteor } from 'meteor/meteor';
 import { loadFixtures } from '/imports/startup/loadFixtures';
 import { RestaurantCollection } from '/imports/api/restaurant.collection';
 import { runPublications } from '/imports/startup/runPublications';
+import { data } from '/db/fixtures';
 
 // SERVER ENTRY POINT
 
 Meteor.startup(async () => {
   if (RestaurantCollection.find().count() === 0) {
     // Prepopulate the db
-    const result = loadFixtures();
+    const result = loadFixtures(data);
 
-    if (result.success) {
-      // Make data available to client
-      runPublications();
-    }
+    if (!result.success) console.error('Error in startup: ', result.msg);
   }
+
+  // Make data available to client
+  runPublications();
 });
