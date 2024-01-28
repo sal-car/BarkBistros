e4d977f3317fa451ea3b78bb1d611aa7a612ddcd
sal-car <sally.carlund@gmail.com>
2024-01-28 15:29:36 +0100

    refactor: move errorboundary component & add secondary palette

--- app/imports/ui/App.tsx
+++ app/imports/ui/App.tsx
@@ -3,6 +3,7 @@ import { Main } from './containers/Main';
 import { CssBaseline } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Header } from './components/Header';
+import { ErrorBoundary } from 'react-error-boundary';
 
 const theme = createTheme({
   palette: {
@@ -10,13 +11,26 @@ const theme = createTheme({
     primary: {
       main: '#FAFAFB',
     },
+    secondary: {
+      main: '#3A8D5C',
+      light: '#FAFAFB',
+      dark: '#3A8D5C',
+    },
   },
 });
 
 export const App = () => (
   <ThemeProvider theme={theme}>
     <CssBaseline />
-    <Header />
-    <Main />
+    <ErrorBoundary
+      fallback={
+        <div className="self-center justify-self-center">
+          Something went wrong
+        </div>
+      }
+    >
+      <Header />
+      <Main />
+    </ErrorBoundary>
   </ThemeProvider>
 );
